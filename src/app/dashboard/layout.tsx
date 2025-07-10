5d1e662992bfc461a7c7b104bcc16c04b83a164d
Damian Ianni <iannigabrieldamian@gmail.com>
2025-07-09 23:38:42 -0300

    Add missing exports and remove unnecessary imports in DashboardLayout component. Page files for each route created

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,17 +2,13 @@
 
 import { AppSidebar } from "@/components/nav/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
-// import { fetchPatientsForUser } from "@/lib/api/mock/patients"; // ejemplo
 import { PageHeader } from "@/components/nav/pageHeader";
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
-  //   BreadcrumbPage,
-  //   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-// import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default async function DashboardLayout({
@@ -29,31 +25,22 @@ export default async function DashboardLayout({
   return (
     <SidebarProvider>
       <AppSidebar />
-      <div className="pl-[--sidebar-width]">
+      <div className="pl-[--sidebar-width] flex min-h-screen w-full flex-col">
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            {/* <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            /> */}
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
                   <BreadcrumbLink href="#">
                     <PageHeader />
-                    {/* Building Your Application */}
                   </BreadcrumbLink>
                 </BreadcrumbItem>
-                {/* <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem> */}
               </BreadcrumbList>
             </Breadcrumb>
           </div>
         </header>
-        <main className="grid flex-col gap-8 p-4 bg-amber-100">{children}</main>
+        <main className="flex-grow p-4 bg-amber-100">{children}</main>
       </div>
     </SidebarProvider>
   );
--- src/components/nav/app-sidebar.tsx
+++ src/components/nav/app-sidebar.tsx
@@ -20,7 +20,7 @@ const data = {
   user: {
     name: "shadcn",
     email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
+    // avatar: Bot,
   },
   teams: [
     {
@@ -75,7 +75,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        {/* <NavProjects projects={data.projects} /> */}
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
