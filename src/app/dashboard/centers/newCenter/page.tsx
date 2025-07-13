ced5b9a80496302f263b96d4c5c526d4138e2a44
Damian Ianni <iannigabrieldamian@gmail.com>
2025-07-13 05:44:20 -0300

    deleted all related to centers

--- src/app/dashboard/centers/newCenter/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EntityForm } from "@/components/forms/entityForm";
-
-export default function NewCenterPage() {
-  return (
-    <div className="flex flex-grow h-full flex-col items-center px-2 py-8 md:px-4">
-      <EntityForm formType="center" />
-    </div>
-  );
-}
