c189df6819d3c43ae0b5a075d6f16958c2177c6a
Damian Ianni <iannigabrieldamian@gmail.com>
2025-07-14 01:46:15 -0300

    hide scrollbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -30,14 +30,14 @@ export default async function DashboardLayout({
     <SidebarProvider>
       {/* Renders the application sidebar. */}
       <AppSidebar />
-      <div className="pl-[--sidebar-width] flex min-h-screen w-full flex-col bg-muted">
+      <div className="pl-[--sidebar-width] flex min-h-screen w-full flex-col bg-muted ">
         <header className=" flex h-10 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-10">
           <div className="flex items-center gap-2 px-2">
             <SidebarTrigger className="-ml-1 cursor-pointer" />
             <PageHeader />
           </div>
         </header>
-        <main className="flex flex-grow p-2 ">
+        <main className="flex flex-grow p-2  ">
           <div className="w-full h-full rounded-xl bg-background">
             {children}
           </div>
